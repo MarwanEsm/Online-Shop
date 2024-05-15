642a261518003c1956f40131a8d1c8137efe73fd
marwanEsm <marwan.esmaail85@gmail.com>
2024-05-15 19:56:12 +0200

    create registration form

--- src/components/layout/forms/registration/Registration.jsx
+++ src/components/layout/forms/registration/Registration.jsx
@@ -1,14 +1,17 @@
+import { Button } from "reactstrap"
 import styles from "./Registration.module.scss"
 
 
 const Registration = () => {
 
-    return <div className={styles.container}>
+    return <form className={styles.container}>
         <input type="text" name="first_name" placeholder="First Name" />
         <input type="text" name="last-name" placeholder="Last Name" />
         <input type="password" name="password" placeholder="Password" />
         <input type="password" name="confirm_password" placeholder="Confirm Password" />
-    </div>
+        <Button color="secondary" size="big" outline>Register</Button>
+
+    </form>
 }
 
 
