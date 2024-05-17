2c013ac4179c36426c916182d4023fae11dda98a
marwanEsm <marwan.esmaail85@gmail.com>
2024-05-17 16:25:16 +0200

    modify style

--- src/components/layout/forms/registration/Registration.jsx
+++ src/components/layout/forms/registration/Registration.jsx
@@ -4,14 +4,15 @@ import styles from "./Registration.module.scss"
 
 const Registration = () => {
 
-    return <form className={styles.container}>
-        <input type="text" name="first_name" placeholder="First Name" />
-        <input type="text" name="last-name" placeholder="Last Name" />
-        <input type="password" name="password" placeholder="Password" />
-        <input type="password" name="confirm_password" placeholder="Confirm Password" />
+    return <div className={styles.container}>
+        <form className={styles.form}>
+            <input type="text" name="first_name" placeholder="First Name" />
+            <input type="text" name="last-name" placeholder="Last Name" />
+            <input type="password" name="password" placeholder="Password" />
+            <input type="password" name="confirm_password" placeholder="Confirm Password" />
+        </form>
         <Button color="secondary" size="big" outline>Register</Button>
-
-    </form>
+    </div>
 }
 
 
